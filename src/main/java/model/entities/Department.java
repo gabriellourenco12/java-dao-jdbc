4a70e7dda434e4ee73ddc830a68e3fe704586376
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-04-07 23:53:57 -0300

    Department entity class

---
+++ src/main/java/model/entities/Department.java
@@ -0,0 +1,47 @@
+package model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Department implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Department other = (Department) obj;
+        if (id == null) {
+            return other.id == null;
+        } else return id.equals(other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department [id=" + id + ", name=" + name + "]";
+    }
+}
