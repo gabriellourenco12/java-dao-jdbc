7da31207607c2089d70ea3e59ece80c1bfbca15d
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-04-08 00:34:31 -0300

    update implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -35,5 +35,11 @@ public class Program {
         Seller newSeller = new Seller(null, "Jack", "jack@gmail.com", new Date(), 7500.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
